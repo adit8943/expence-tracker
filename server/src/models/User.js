6551d4387e2b27d4332dc361b05058097466f928
Aditya <your_email@gmail.com>
2026-08-16 00:50:38 +0530

    fix: support promise-based password hashing hook

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -7,10 +7,9 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, minlength: 6, select: false },
 }, { timestamps: true });
 
-userSchema.pre('save', async function hashPassword(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function hashPassword() {
+  if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 12);
-  next();
 });
 
 userSchema.methods.comparePassword = function comparePassword(candidate) {
